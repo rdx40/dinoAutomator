112e8ed5a22b0252135da284887ef24a66b32ee4
rdx40 <138113713+rdx40@users.noreply.github.com>
2023-11-06 11:51:08 +0530

    Update dino.py

--- dino.py
+++ dino.py
@@ -1,6 +1,5 @@
-import pyautogui # pip install pyautogui
-from PIL import Image, ImageGrab # pip install pillow
-# from numpy import asarray
+import pyautogui 
+from PIL import Image, ImageGrab 
 import time
 
 def hit(key):
@@ -8,7 +7,7 @@ def hit(key):
     return
 
 def isCollide(data):
-    # Draw the rectangle for birds
+    
     for i in range(300, 415):
         for j in range(410, 563):
             if data[i, j] < 100:
@@ -32,14 +31,11 @@ if __name__ == "__main__":
         data = image.load()
         isCollide(data)
             
-        # print(asarray(image))
         '''
-        # Draw the rectangle for cactus
         for i in range(275, 325):
             for j in range(563, 650):
                 data[i, j] = 0
         
-        # Draw the rectangle for birds
         for i in range(250, 300):
             for j in range(410, 563):
                 data[i, j] = 171
